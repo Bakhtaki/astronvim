f9bf6baa7f5f08347f5b62381797812e65b12f9a
Bakhtaki <hamidreza.bakhtaki@gmail.com>
2023-12-28 10:25:11 +0330

    Parsers Added to treesitter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,6 +5,11 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
       "python",
+      "cpp",
+      "rust",
+      "bash",
+      "yaml",
+      "json",
     })
   end,
 }
